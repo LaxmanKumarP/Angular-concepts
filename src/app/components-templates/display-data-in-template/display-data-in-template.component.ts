8941b7ca70b115cc53e160b183129a7b2e5d1baf
Laxman Kumar <laxman.apr9@gmail.com>
2020-09-03 22:03:39 +0530

    initial setup of page design

---
+++ src/app/components-templates/display-data-in-template/display-data-in-template.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-data-in-template',
+  template: `<p>{{title}}</p>
+             <p>{{myHero}}</p>`
+})
+export class DisplayDataInTemplateComponent implements OnInit {
+
+  title = 'Tour of Heroes';
+  myHero = 'Windstorm';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
