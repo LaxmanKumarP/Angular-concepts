ac74a4fa91b3b9bc382cf00be8b88d09080f92e3
Laxman Kumar <laxman.apr9@gmail.com>
2020-09-16 21:36:14 +0530

    templatesyntax

--- src/app/components-templates/template-syntax/template-syntax.component.ts
+++ src/app/components-templates/template-syntax/template-syntax.component.ts
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateSyntaxComponent implements OnInit {
 
+  currentHero = {
+    name: "Steven ",
+    power: 200
+  };
+
+  heroImageUrl = './assets/images/hero.jpg';
+  heroImageUrl1 = './assets/images/hero3.jpg';
+  formNotChanged = true;
+  evilTitle = 'Template <script>alert("evil never sleeps")</script>Syntax';
+
+
+  canSave = true;
+  isUnchanged = false;
+  isSpecial = false;
+
+  classConditions =
+    {
+      'saveable': this.canSave,
+      'modified': !this.isUnchanged,
+      'special': this.isSpecial
+    };
+
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getVal() {
+    return 1;
+  }
+  getHeroImg() {
+    return this.heroImageUrl;
   }
 
 }
